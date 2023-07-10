c2830ce39cd2b6a4a7a44bbbc6fabe8d56779eb7
gerpi <gerpi.2666@gmail.com>
2023-07-10 08:46:29 -0600

    fix api like facebook template

--- app.js
+++ app.js
@@ -98,18 +98,32 @@ const main = async () => {
         console.log('Incoming webhook: ' + JSON.stringify(request.body));
         response.sendStatus(200);
     });
-    app.get("/webhook", function (req, res) {
-        console.log(req)
-        let accessToken = "ASD54858ASDEDRFEWF";
-        let  token = req.query["hub.verify_token"];
+    app.get("/webhook", (req, res) => {
+        /**
+         * UPDATE YOUR VERIFY TOKEN
+         *This will be the Verify Token value when you set up webhook
+        **/
+        const verify_token = process.env.VERIFY_TOKEN;
+      
+        // Parse params from the webhook verification request
+        let mode = req.query["hub.mode"];
+        let token = req.query["hub.verify_token"];
         let challenge = req.query["hub.challenge"];
-
-        if(challenge != null && token != null && token == accessToken){
+      
+        // Check if a token and mode were sent
+        if (mode && token) {
+          // Check the mode and token sent are correct
+          if (mode === "subscribe" && token === verify_token) {
+            // Respond with 200 OK and challenge token from the request
+            console.log("WEBHOOK_VERIFIED");
             res.status(200).send(challenge);
-        }else{
-            res.status(400).send();
+          } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+          }
         }
       });
+      
 
   
 
